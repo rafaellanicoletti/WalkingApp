0e975d137e266c2ed7c584eae6c08975acfff36d
anahisbarrie <anahis.barrie@gmail.com>
2019-09-11 19:34:33 -0400

    adding map to home page

--- prowalkweb/src/App.js
+++ prowalkweb/src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {GoogleMap, withScriptjs, withGoogleMap} from "react-google-maps"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function Map(){
+  return (<GoogleMap defaultZoom={10} defaultCenter={{ lat: 40.730610, lng: -73.935242}}
+  />
   );
 }
+const WrappedMap = withScriptjs(withGoogleMap(Map))
 
-export default App;
+export default function App(){
+  return(
+    <div style={{width:'100vw', height:'100vh'}}>
+      <WrappedMap googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBhHbOzEagXSNiH8ijFeZ415GRo3unB7U4`}
+    loadingElement ={<div style={{height:"100%" }}/>}
+    containerElement={<div style={{ height: "100%" }} />}
+     mapElement={<div style={{ height: "100%" }} />}
+    />
+  
+  </div>
+  );
+}
