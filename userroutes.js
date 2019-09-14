8f714aee6c1e79f8e8cb03874694c049b9b8972c
rafaella nicoletti <rafaella_np@hotmail.com>
2019-09-14 13:39:49 -0400

    user routes and usser model

---
+++ userroutes.js
@@ -0,0 +1,18 @@
+const express = require ('express');
+const router = express.Router();
+const mongoose = require ('mongoose');
+
+const User= = require ('...models file user file');
+
+router.post('/signup' , (req,res) => {
+    const user = new User
+
+});
+
+
+
+
+
+
+
+module.exports = router;
