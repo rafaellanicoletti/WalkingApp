bcf3792967a87432221ee29e936ee67e1168719d
rafaella nicoletti <rafaella_np@hotmail.com>
2019-09-18 10:48:14 -0400

    login component

---
+++ prowalkweb/src/login.js
@@ -0,0 +1,14 @@
+import React , { Component } from 'react';
+import axios from 'axios';
+
+export default class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email : "",
+            password : "",
+            loginErrors : ""
+        };
+    }
+}
---
+++ routes/userroute.js
@@ -0,0 +1,54 @@
+const express = require ('express');
+const router = express.Router();
+const mongoose = require ('mongoose');
+
+//const bcrypt = require ('bcrypt');
+
+const User= = require ('...models file user file');
+
+router.post('/signup' , (req,res) => {
+
+    // bcrypt.hash(req.body.email, 10, (err, hash) => {
+            //if (err){
+                //return res.status(500).json({
+                    //error: err
+                // })
+            // } else {
+                // 
+            // }
+
+    const user = new User ({
+        _id: new mongoose.Types.ObjectId(),
+        email: req.body.email,
+        password: req.body.password 
+        //node bcrypt to hash the password, will be encrypted npm i bcrypt
+        //password : 
+        })
+
+        user.save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message: 'User created'
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error:err
+            })
+        });
+
+    })
+
+// });
+
+//NEED TO IMPORT USER ROUTES IN APP.JS
+
+
+
+
+
+
+
+module.exports = router;
