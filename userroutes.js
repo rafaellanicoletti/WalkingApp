72c1beae932d35d26beeb4f66a2837a0a169b461
rafaella nicoletti <rafaella_np@hotmail.com>
2019-09-18 09:56:49 -0400

    routes and user log in

--- userroutes.js
+++ userroutes.js
@@ -5,7 +5,14 @@ const mongoose = require ('mongoose');
 const User= = require ('...models file user file');
 
 router.post('/signup' , (req,res) => {
-    const user = new User
+    const user = new User ({
+        _id: new mongoose.Types.ObjectId(),
+        email: req.body.email,
+        password: req.body.password 
+        //node bcrypt to hash the password, will be encrypted npm i bcrypt
+        
+
+    })
 
 });
 
