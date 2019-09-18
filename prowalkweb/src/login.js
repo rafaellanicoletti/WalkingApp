990cc9ad83c55cfc83d6f7a0b5b4b47a6ab5f0e4
rafaella nicoletti <rafaella_np@hotmail.com>
2019-09-18 10:56:38 -0400

    catch error

--- prowalkweb/src/login.js
+++ prowalkweb/src/login.js
@@ -40,7 +40,7 @@ export default class Login extends Component {
                     this.props.handleSuccessfulAuth(response.data);
                 }
             })
-            .catch
+            .catch(error =>)
     }
 }
 
