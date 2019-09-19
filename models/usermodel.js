5be806de3e60d7f451c14f27790796ffbea73793
rafaella nicoletti <rafaella_np@hotmail.com>
2019-09-18 21:04:26 -0400

    signup login delet done

--- models/usermodel.js
+++ models/usermodel.js
@@ -4,7 +4,8 @@ const mongoose = require ("mongoose");
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     email: { type: String, required: true},
-    password: { type: String, required: true}
+    password: { type: String, required: true},
+    isLoggedIn: { type: Boolean, default: false}
 });
 
 module.exports = mongoose.model('User', userSchema);
--- routes/userroute.js
+++ routes/userroute.js
@@ -7,7 +7,8 @@ app.post('/signup' , (req,res) => {
     const user = new User ({
         _id: new mongoose.Types.ObjectId(),
         email: req.body.email,
-        password: req.body.password 
+        password: req.body.password,
+        isLoggedIn: false
         //node bcrypt to hash the password, will be encrypted npm i bcrypt
         //password : 
         })
@@ -29,17 +30,36 @@ app.post('/signup' , (req,res) => {
     })
 
     app.post("/login", (req,res)=> {
-        User.find({ email: req.body.email})
+        User.find({ email: req.body.email , password: req.body.password})
         .exec()
         .then(user => {
+            console.log(user);
             if (user.length < 1){
                 return res.status(404).json({
                     message: "Mail not found"
                 });
+            } else{
+                let query= {email: user[0].email, password: user[0].password};
+                console.log("query", query)
+                User.update(query, {isLoggedIn: true})
+       
+                // User.save()
+                    .then(result => {
+                        console.log(result);
+                        res.status(200).json({
+                            message: 'User logged in!!'
+                        })
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        res.status(500).json({
+                            error: err
+                        })
+                    });
             }
-            
+           
         })
-    })
+    });
 
     app.delete("/:userId", (req,res)=> {
         User.remove({ _id: req.params.userId})
