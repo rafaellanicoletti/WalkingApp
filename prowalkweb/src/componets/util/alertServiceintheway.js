2479e6657e566673ee8bd94ee1e6aa9b16f3ee59
anahisbarrie <anahis.barrie@gmail.com>
2019-09-22 21:51:07 -0400

    adding rating buttom

--- prowalkweb/src/componets/util/alertServiceintheway.js
+++ prowalkweb/src/componets/util/alertServiceintheway.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { confirmAlert } from 'react-confirm-alert'; // Import
 import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
+import mobiscroll from '@mobiscroll/react-lite';
+
+
+mobiscroll.settings = {
+    theme: 'ios'
+}
+
 
 export default class App extends React.Component {
     submit = () => {
@@ -20,11 +27,39 @@ export default class App extends React.Component {
         });
     };
 
+    showConfirm() {
+        mobiscroll.confirm({
+            title: 'Please Rate your Walker',
+            message: 'How did you like the service?',
+            okText: 'Agree',
+            cancelText: 'Disagree',
+
+            // Add a rating function here 
+            callback: (res) => {
+                mobiscroll.toast({
+                    message: res ? 'Agreed' : 'Disagreed'
+                });
+            }
+        });
+    }
+
     render() {
         return (
             <div className='container'>
                 <button onClick={this.submit}>Book Now</button>
+                <mobiscroll.Form>
+                    <mobiscroll.FormGroup>
+                        <mobiscroll.FormGroupTitle></mobiscroll.FormGroupTitle>
+                        <div className="mbsc-btn-group-block">
+                            {/* <mobiscroll.Button onClick={this.showAlert}>Alert</mobiscroll.Button> */}
+                            <mobiscroll.Button onClick={this.showConfirm}>Finish Service</mobiscroll.Button>
+                            {/* <mobiscroll.Button onClick={this.showPrompt}>Prompt</mobiscroll.Button> */}
+                        </div>
+                    </mobiscroll.FormGroup>
+                </mobiscroll.Form>
             </div>
+            
+           
         );
     }
 }
